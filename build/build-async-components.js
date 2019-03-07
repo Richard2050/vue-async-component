20cfdaa262ce2d3b22e5c10dc3ec1220c2956446
RichardMiao <miaochuanxing@126.com>
2019-03-07 09:27:40 +0800

    清理不用的依赖

--- build/build-async-components.js
+++ build/build-async-components.js
@@ -4,7 +4,6 @@ require('./check-versions')()
 process.env.NODE_ENV = 'production'
 
 const ora = require('ora')
-const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.async-components.prod.conf')
@@ -12,25 +11,29 @@ const webpackConfig = require('./webpack.async-components.prod.conf')
 const spinner = ora('building for sync-components...')
 spinner.start()
 
-webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
-  if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n\n')
+webpack(webpackConfig, function(err, stats) {
+    spinner.stop()
+    if (err) throw err
+    process.stdout.write(
+        stats.toString({
+            colors: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
+        }) + '\n\n'
+    )
 
-  if (stats.hasErrors()) {
-    console.log(chalk.red('  Build failed with errors.\n'))
-    process.exit(1)
-  }
+    if (stats.hasErrors()) {
+        console.log(chalk.red('  Build failed with errors.\n'))
+        process.exit(1)
+    }
 
-  console.log(chalk.cyan('  Build complete.\n'))
-  console.log(chalk.yellow(
-    '  Tip: built files are meant to be served over an HTTP server.\n' +
-    '  Opening index.html over file:// won\'t work.\n'
-  ))
+    console.log(chalk.cyan('  Build sync-components complete.\n'))
+    console.log(
+        chalk.yellow(
+            '  Tip: built files are meant to be served over an HTTP server.\n' +
+                "  Opening index.html over file:// won't work.\n"
+        )
+    )
 })
